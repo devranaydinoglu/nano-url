d0f989283a64ef512126f9207a903af596ba5155
devranaydinoglu <d.s.aydinoglu@gmail.com>
2026-08-07 16:43:12 +0200

    Added GET endpoint for url entity

--- src/main/java/com/nanourl/nanourl/url/UrlController.java
+++ src/main/java/com/nanourl/nanourl/url/UrlController.java
@@ -1,8 +1,11 @@
 package com.nanourl.nanourl.url;
 
+import com.nanourl.nanourl.util.ApiResponse;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
 
 @RestController
 @RequestMapping("/urls")
@@ -14,10 +17,25 @@ public class UrlController {
         this.urlService = urlService;
     }
 
+    @GetMapping("/{shortCode}")
+    public ResponseEntity<ApiResponse<UrlResponse>> findByShortCode(@PathVariable String shortCode) {
+        UrlResponse foundUrl = urlService.findByShortCode(shortCode);
+
+        if (foundUrl == null)
+            return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .body(ApiResponse.error("URL not found"));
+
+        return ResponseEntity
+            .status(HttpStatus.OK)
+            .body(ApiResponse.success(foundUrl, "URL retrieved successfully"));
+    }
+
     @PostMapping
-    @ResponseStatus(HttpStatus.CREATED)
-    public CreateUrlResponse create(@RequestBody @Valid CreateUrlRequest req) {
-        return urlService.create(req);
+    public ResponseEntity<ApiResponse<CreateUrlResponse>> create(@RequestBody @Valid CreateUrlRequest req) {
+        CreateUrlResponse response = urlService.create(req);
+        return ResponseEntity.status(HttpStatus.CREATED)
+            .body(ApiResponse.success(response, "Short URL created successfully"));
     }
 
 }
