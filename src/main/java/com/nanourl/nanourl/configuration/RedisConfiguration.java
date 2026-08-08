b92de3cf3b31b6a30101790f0d840aaddd004c41
devranaydinoglu <d.s.aydinoglu@gmail.com>
2026-08-08 19:12:14 +0200

    Created Redis configuration

---
+++ src/main/java/com/nanourl/nanourl/configuration/RedisConfiguration.java
@@ -0,0 +1,28 @@
+package com.nanourl.nanourl.configuration;
+
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+
+import java.time.Duration;
+
+@Configuration
+@EnableCaching
+public class RedisConfiguration {
+
+    @Bean
+    public RedisCacheManager cacheManager(RedisConnectionFactory connectionFactory) {
+        RedisCacheConfiguration cacheConfiguration = RedisCacheConfiguration.defaultCacheConfig()
+            .entryTtl(Duration.ofMinutes(20))
+            .disableCachingNullValues();
+
+        return RedisCacheManager
+            .builder(connectionFactory)
+            .cacheDefaults(cacheConfiguration)
+            .build();
+    }
+
+}
